7a55f037022e760bb43eac5c15d1adf055a0938a
Abdul-Rahman Ibrahim <abdulrahmanibrahim.ish@gmail.com>
2026-06-25 22:18:18 +0300

    configured templates, statics and media

--- _booking/settings.py
+++ _booking/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = '_booking.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,3 +128,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
