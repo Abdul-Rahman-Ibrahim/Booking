2eaa9d51dd98b59dc777f69cb50fe2e5eeb93535
Abdul-Rahman Ibrahim <abdulrahmanibrahim.ish@gmail.com>
2026-06-25 22:15:43 +0300

    set up calendar view

--- main/views.py
+++ main/views.py
@@ -1,3 +1,10 @@
+# pyrefly: ignore [missing-import]
 from django.shortcuts import render
+# pyrefly: ignore [missing-import]
+from django.views.generic import View 
 
 # Create your views here.
+
+class CalendarView(View):
+    def get(self, request):
+        return render(request, 'main/calendar.html')
