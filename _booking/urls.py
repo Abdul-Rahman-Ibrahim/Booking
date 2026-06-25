363d6794e5bad90c0f300dc8b4ae8764f1726c27
Abdul-Rahman Ibrahim <abdulrahmanibrahim.ish@gmail.com>
2026-06-25 21:26:09 +0300

    ignored import error

--- _booking/urls.py
+++ _booking/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+
+
+from django.contrib import admin # pyrefly: ignore [missing-import]
+from django.urls import path # pyrefly: ignore [missing-import]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
