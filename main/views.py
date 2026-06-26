6d984755609374e397aad30a08e2ac79513620ed
Abdul-Rahman Ibrahim <abdulrahmanibrahim.ish@gmail.com>
2026-06-26 11:26:15 +0300

    added active page context

--- main/views.py
+++ main/views.py
@@ -3,23 +3,34 @@ from django.shortcuts import render
 # pyrefly: ignore [missing-import]
 from django.views.generic import View 
 
-# Create your views here.
 
 class HomePageView(View):
     def get(self, request):
-        return render(request, 'main/index.html')
+        context = {
+            'active_page': 'calendar',
+        }
+        return render(request, 'main/index.html', context)
 
 
 class EquipmentPageView(View):
     def get(self, request):
+        context = {
+            'active_page': 'equipment',
+        }
         return render(request, 'main/equipment.html')
 
 
 class SettingsPageView(View):
     def get(self, request):
+        context = {
+            'active_page': 'settings',
+        }
         return render(request, 'main/settings.html')
 
 
 class BookingListView(View):
     def get(self, request):
+        context = {
+            'active_page': 'bookings',
+        }
         return render(request, 'main/bookings.html')
